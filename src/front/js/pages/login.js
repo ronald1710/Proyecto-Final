2dfd32311d52578edc9a944de5bbaae71de9495a
ronald1710 <ronald.azofeifa@yahoo.com>
2022-11-01 01:32:18 +0000

    files, login, biblioteca, home

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import imagen_login from "../../img/imagen_login.png";
+import imagen_login from "./../../img/fondo_imagen.jpg";
 import { useNavigate } from "react-router-dom";
 import "../../styles/login.css";
 
@@ -18,16 +18,28 @@ export const Login = () => {
   };
 
   return (
-    <div id="div_fondo">
-      <div>
-        <img
-          /*src={imagen_login}*/
-          className="img-fluid rounded-start"
-          alt="imagen_login"
-          id="imagen_login"
-        />
+    <div
+      id="div_fondo"
+      style={{ backgroundImage: `url(${imagen_login})`, height: "490px" }}
+    >
+      <div className="text-center ">
+        <h1>Acceso</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClick}>Acceso</button>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
