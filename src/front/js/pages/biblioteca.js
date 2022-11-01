8c246b9f22cdb19087ed0da4a7161b42fcbf2f15
ronald1710 <ronald.azofeifa@yahoo.com>
2022-11-01 00:57:47 +0000

    3

--- src/front/js/pages/biblioteca.js
+++ src/front/js/pages/biblioteca.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Card } from "../component/cards";
 import { Context } from "../store/appContext";
+import "../../styles/biblioteca.css";
 
 export const Biblioteca = () => {
   const { store, actions } = useContext(Context);
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import fondo_login from "../../img/fondo_login.jpg";
+import imagen_login from "../../img/imagen_login.png";
 import { useNavigate } from "react-router-dom";
 import "../../styles/login.css";
 
@@ -19,24 +19,15 @@ export const Login = () => {
 
   return (
     <div id="div_fondo">
-      <div className="text-center">
-        <h1>Acceso</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={handleClick}>Acceso</button>
-        </div>
+      <div>
+        <img
+          /*src={imagen_login}*/
+          className="img-fluid rounded-start"
+          alt="imagen_login"
+          id="imagen_login"
+        />
       </div>
+      <div></div>
     </div>
   );
 };
