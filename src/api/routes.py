a049c4bc62e1dcb963aa681e3970df6ba1a5e5ad
ImFineZ <greivinrodriguez@icloud.com>
2022-11-03 02:18:56 +0000

    Cambio facherito

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,37 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
+@api.route("/login", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user= User.query.filter_by(email=email,password=password).first()
+    if not user:
+        return jsonify({"msg": "Not exits user"}), 401
+    
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/signup', methods=['POST'])
+def handle_Signup():
+    
+    email= request.json.get("email", None)
+    password = request.json.get("password", None)
+    name = request.json.get("name", None)
 
-    return jsonify(response_body), 200
+    user= User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"msg":"Este email ya existe"})
+    user_new = User(email=email,password=password, name=name)
+    db.session.add(user_new)
+    db.session.commit()
+    return jsonify({"msg":"Registrado existosamente"}), 200
+
+
+
+
