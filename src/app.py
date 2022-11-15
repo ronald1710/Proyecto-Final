53de0d0b3fb9227f01f212c6da0c466605b8749e
rivervil16 <agustinriverovila@gmail.com>
2022-11-15 02:50:22 +0000

    cambios en correo

--- src/app.py
+++ src/app.py
@@ -45,27 +45,10 @@ mail_settings = {
     # MAIL_ASCII_ATTACHMENTS : default False
 }
 
-""" port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "mypetfriendf@gmail.com"
-receiver_email = "abelgr45@gmail.com"
-password ="lcqwkbumcbotlngy"
-message = """
-# \"""
-# Subject: Hi there
-# This message is sent from Python."""
-""" context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
- """ """
 app.config.update(mail_settings)
 mail = Mail(app)
 # se agrega mail a la app y se va a llamar en routes.py como current_app
-app.mail = mail"""
+app.mail = mail
 
 
 # database condiguration
